55dac7e194fef8a6d9b67932f84c1e0270da93f2
AstraBert <astraberte9@gmail.com>
2024-04-30 01:45:10 +0200

    first commit

---
+++ article1/bot.py
@@ -0,0 +1,47 @@
+from telegram.ext import *
+
+TOKEN = "6917358471:AAELOdwHi_PV_YJqRN-qGAq9BmUFYi6NYmc"
+
+async def start_command(update, context):
+    await update.message.reply_text("Hi, I'm NeapolitanPizzaBot, how may I assist you today?")
+
+async def price_command(update, context):
+    await update.message.reply_text("- Margherita: 8$\n- Marinara: 5$\n- Pepperoni: 9$")
+
+async def location_command(update, context):
+    await update.message.reply_venue(40.82694507320722, 14.426713826679295, "Italy", "Pizzeria NeapolitanPizzaBot")
+
+async def menu_command(update, context):
+    await update.message.reply_photo("menu.jpg")
+
+async def unrecognized_command(update, context):
+    text = update.message.text
+    if (
+        text.startswith("/start") == False
+        or text.startswith("/menu") == False
+        or text.startswith("/price") == False
+        or text.startswith("/location") == False
+    ):
+        await update.message.reply_text(
+            f'I cannot understand the message:\n"{text}"\nAs my programmer did not insert it among the command I am set to respond (that are /price, /menu and /location): please check for misspelling/errors or contact the programmer if you feel anything is wrong/missing'
+        )
+    else:
+        pass
+
+
+async def error_handler(update, context: CallbackContext) -> None:
+    await update.message.reply_text("Sorry, something went wrong.")
+
+if __name__ == "__main__":
+    print("Bot is high and running")
+    application = Application.builder().token(TOKEN).build()
+    # Commands
+    application.add_handler(CommandHandler("start", start_command))
+    application.add_handler(CommandHandler("menu", menu_command))
+    application.add_handler(CommandHandler("price", price_command))
+    application.add_handler(CommandHandler("location", location_command))
+    application.add_handler(MessageHandler(filters.COMMAND, unrecognized_command))
+    application.add_handler(MessageHandler(filters.TEXT, unrecognized_command))
+    application.add_error_handler(error_handler)
+    # Run bot
+    application.run_polling(1.0)
---
+++ article2/ml_framework.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv("italian_cities.csv")
+
+X = df.drop(columns=["Destination"])
+y = list(df["Destination"])
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+
+from sklearn.tree import DecisionTreeClassifier
+
+model = DecisionTreeClassifier()
+model = model.fit(X_train, y_train)
+
+
+from sklearn.metrics import accuracy_score, classification_report
+y_pred = model.predict(X_test)
+print(f"Accuracy is {accuracy_score(y_test, y_pred)}")
+print(f"Classification report:\n{classification_report(y_test,y_pred)}")
